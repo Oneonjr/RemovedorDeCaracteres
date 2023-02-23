249e19d5e691b3f5582c301502abf4483d7c35bd
Oneon Junior <oneonjunior@outlook.com>
2023-02-23 20:13:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function removeSpecialCharacters() {
     let output = input.replace(/[^\w\s]/gi, '');
     document.getElementById("outputText").innerHTML = output;
   }
+
+const RemoverComEnter = document.getElementById('inputText');
+
+  RemoverComEnter.addEventListener('keypress', (remover)=>{
+    if (remover.key === 'Enter') {
+      removeSpecialCharacters();
+    }
+  })
